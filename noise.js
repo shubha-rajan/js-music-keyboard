ad8ba33bdd9376dfeccd9bb3171e2440030f7bad
Shubha Rajan <shubharajan94@gmail.com>
2019-05-28 18:21:05 -0700

    added pause and reload to event listeners. can play chords and songs now!'

--- noise.js
+++ noise.js
@@ -3,12 +3,16 @@ $(document).ready( function() {
   $('.instrument').on('click', 'button', async function(event) {
     classArray = $(this).attr("class").split(' ');
     const note = _.last(classArray)
+    $(`#${note}Audio`).get(0).pause();
+    $(`#${note}Audio`).get(0).load();
     $(`#${note}Audio`).get(0).play();
   });
-  $('body').keypress(async function(event) {
+  $('body').keyup(async function(event) {
     note = event.key;
     const notes = ['c', 'd', 'e', 'f', 'g', 'a', 'b'];
     if (notes.includes(event.key)) {
+      $(`#${note}Audio`).get(0).pause();
+      $(`#${note}Audio`).get(0).load();
       $(`#${note}Audio`).get(0).play();
     }
   });
