4aaf43df27dc37ed80d4abb71f9dce95ecc68770
Shubha Rajan <shubharajan94@gmail.com>
2019-05-28 18:10:45 -0700

    made functions asynchronous

--- noise.js
+++ noise.js
@@ -1,15 +1,11 @@
 $(document).ready( function() {
-  $('.instrument').on('click', 'button', function(event) {
+
+  $('.instrument').on('click', 'button', async function(event) {
     classArray = $(this).attr("class").split(' ');
     const note = _.last(classArray)
     $(`#${note}Audio`).get(0).play();
   });
-  $('.instrument').on('click', 'button', function(event) {
-    classArray = $(this).attr("class").split(' ');
-    const note = _.last(classArray)
-    $(`#${note}Audio`).get(0).play();
-  });
-  $('body').keypress((event) => {
+  $('body').keypress(async function(event) {
     note = event.key;
     const notes = ['c', 'd', 'e', 'f', 'g', 'a', 'b'];
     if (notes.includes(event.key)) {
