dfc68242bdc6cd2575cc62d64a120c0bdf6603ff
Shubha Rajan <shubharajan94@gmail.com>
2019-05-28 17:51:09 -0700

    keyboard works with clicks and keypress

--- noise.js
+++ noise.js
@@ -1,3 +1,19 @@
 $(document).ready( function() {
-  // your code here
+  $('.instrument').on('click', 'button', function(event) {
+    classArray = $(this).attr("class").split(' ');
+    const note = _.last(classArray)
+    $(`#${note}Audio`).get(0).play();
+  });
+  $('.instrument').on('click', 'button', function(event) {
+    classArray = $(this).attr("class").split(' ');
+    const note = _.last(classArray)
+    $(`#${note}Audio`).get(0).play();
+  });
+  $('body').keypress((event) => {
+    note = event.key;
+    const notes = ['c', 'd', 'e', 'f', 'g', 'a', 'b'];
+    if (notes.includes(event.key)) {
+      $(`#${note}Audio`).get(0).play();
+    }
+  });
 });
